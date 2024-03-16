7b55a50e0e298c2b7de09336b0e533912962efcc
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-16 15:45:20 +0300

    123

---
+++ Angle/Class1.cs
@@ -0,0 +1,81 @@
+﻿namespace Angle;
+
+public class Angle
+{
+    double gradus;
+    double min;
+    double sec;
+    bool isCorrect=true;
+
+    public double Gradus
+    {
+        get
+        {
+            return gradus;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Значение некорректно!");
+                isCorrect = false;
+
+            }
+
+            else gradus = value;
+        }
+    }
+    public double Min
+    {
+        get
+        {
+            return min;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Значение некорректно!");
+                isCorrect = false;
+
+            }
+
+            else min = value;
+        }
+    }
+    public double Sec
+    {
+        get
+        {
+            return sec;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Значение некорректно!");
+                isCorrect = false;
+
+            }
+
+            else sec = value;
+        }
+    }
+
+    public Angle (int gradus, int min, int sec)
+    {
+        this.Gradus = gradus;
+        this.Min = min;
+        this.Sec = sec;
+ 
+    }
+    public double ToRadian(int gradus, int min, int sec)
+    {
+        return gradus * (Math.PI / 180)+(min/60)/(Math.PI / 180)+ (sec / 3600) / (Math.PI / 180);
+    }
+
+    
+}
+
+
+
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Task1;
-
+﻿
+namespace Task1;
+//Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут,
+//sec - угловых секунд. Реализовать класс, в котором указанные значения представлены в виде свойств.
+//Для свойств предусмотреть проверку корректности данных.
+//Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы.
+//Создать объект на основе разработанного класса.
+//Осуществить использование объекта в программе.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Angle angle;
+        do
+        {
+            Console.WriteLine("Введите градусы");
+            double gr = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите минуты");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите секунды");
+            double s = Convert.ToDouble(Console.ReadLine());
+
+            angle = new Angle(gr, m, s);
+        }
+        while (angle.isCorrect == false);
+
+        double all_in_rad = angle.ToRadian();
+        Console.WriteLine($"В угле {all_in_rad} радиан");
+        Console.ReadKey();
+
+       
+
+
+        
+        
     }
 }
 
